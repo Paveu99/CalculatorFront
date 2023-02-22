2009a69700d5c1f04a7e148b9fc673fd7b0cd22d
Pawel <pawel@example.com>
2023-02-22 17:20:50 +0100

    Added views

---
+++ src/views/CreateFormView.tsx
@@ -0,0 +1,4 @@
+import React from "react";
+import {AddExpression} from "../components/AddExpression/AddExpression";
+
+export const CreateFormView = () => (<AddExpression/>)
---
+++ src/views/MathHistoryView.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import {ExpressionList} from "../components/Expressions/ExpressionList";
+
+export const MathHistoryView = () => (
+    <>
+        <ExpressionList/>
+    </>
+)
